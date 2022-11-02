e397ca636df6b6ec0b56df5465c8c1e18d2396e9
timyouell-servian <tim.youell@servian.com>
2022-11-02 12:06:24 +1100

    data appending attempt1

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import datetime as dt
+import csv
 
 base_url = 'https://fantasy.premierleague.com/api/'
 
@@ -44,9 +45,35 @@ def get_trans_in():
 
 # run via GitHub Actions
 
+def get_data_row():
+    ele_df = pd.DataFrame(get_bootstrap_data()['elements'])
+    eh_df = ele_df.loc[ele_df['second_name'] == 'Haaland']
+    data = {'Name': [eh_df['second_name'].iloc[0]],
+            'TSB%': [eh_df['selected_by_percent'].iloc[0]],
+            'T_In_GW': [eh_df['transfers_in_event'].iloc[0]],
+            'T_Out_GW': [eh_df['transfers_out_event'].iloc[0]],
+            'datetime': dt.datetime.now()}
+    return data
+
+
+def append_data():
+    with open('data/haaland_transfers.csv', "r") as f:
+        reader = csv.reader(f)
+        for header in reader:
+            break
+    
+    myDict = get_data_row()
+    # add row to CSV file
+    with open('data/haaland_transfers.csv', "a", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=header)
+        writer.writerow(myDict)
+    
+
 def main():
     t_in = get_trans_in()
     print('Total transfers in for Erling Haaland this GW is: ' + str(t_in))
+    append_data()
+    print('Data appended!')
     
 
 if __name__ == "__main__":
